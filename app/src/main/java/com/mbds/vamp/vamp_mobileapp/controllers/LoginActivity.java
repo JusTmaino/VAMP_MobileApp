0ae0fa67c80a009256ab66df25401eb2f7a1b844
Nour BENMOUSSA <nei.benmoussa@gmail.com>
2017-12-16 17:33:15 +0100

    #1 #2 #3 #4 #5 en cours

--- app/src/main/java/com/mbds/vamp/vamp_mobileapp/controllers/LoginActivity.java
+++ app/src/main/java/com/mbds/vamp/vamp_mobileapp/controllers/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 LOGIN = true;
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, AppActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/mbds/vamp/vamp_mobileapp/controllers/fragments/ProfileFragment.java
+++ app/src/main/java/com/mbds/vamp/vamp_mobileapp/controllers/fragments/ProfileFragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 import com.mbds.vamp.vamp_mobileapp.R;
 
 
-public class SettingsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         return rootView;
     }
 
